1abcf71b76e761f4ab1649e1a4e9cd7f113b5388
Ght-139 <ght139439@qq.com>
2024-03-06 22:01:41 +0800

    api搜素密钥更改

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -112,10 +112,51 @@ export default defineUserConfig({
   }),
   // 搜索插件
   docsearchPlugin({
-    appId: "150XP8FDHT",
-    apiKey: "a816afc935fb6dc08c54a0e66b0ee529",
+    appId: "15OXP8FDHT",
+    apiKey: "8c63da609ed28c67f21649a05aba8855",
     indexName: "nulizongyouhuibao",
     locales: {
+      "/": {
+        placeholder: "搜索内容",
+        translations: {
+          button: {
+            buttonText: "搜索",
+            buttonAriaLabel: "搜索",
+          },
+          modal: {
+            searchBox: {
+              resetButtonTitle: "清除查询条件",
+              resetButtonAriaLabel: "清除查询条件",
+              cancelButtonText: "取消",
+              cancelButtonAriaLabel: "取消",
+            },
+            startScreen: {
+              recentSearchesTitle: "搜索历史",
+              noRecentSearchesText: "没有搜索历史",
+              saveRecentSearchButtonTitle: "保存至搜索历史",
+              removeRecentSearchButtonTitle: "从搜索历史中移除",
+              favoriteSearchesTitle: "收藏",
+              removeFavoriteSearchButtonTitle: "从收藏中移除",
+            },
+            errorScreen: {
+              titleText: "无法获取结果",
+              helpText: "你可能需要检查你的网络连接",
+            },
+            footer: {
+              selectText: "选择",
+              navigateText: "切换",
+              closeText: "关闭",
+              searchByText: "搜索提供者",
+            },
+            noResultsScreen: {
+              noResultsText: "无法找到相关结果",
+              suggestedQueryText: "你可以尝试查询",
+              reportMissingResultsText: "你认为该查询应该有结果？",
+              reportMissingResultsLinkText: "点击反馈",
+            },
+          },
+        },
+      },
       "/zh/": {
         placeholder: "搜索内容",
         translations: {
